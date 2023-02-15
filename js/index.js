5212910bee54b0af4731fa04f2c32f4ed4e76479
Alex Brahos <alexanderbrahos@gmail.com>
2023-02-15 16:45:24 -0500

    finished header and added comments

--- js/index.js
+++ js/index.js
@@ -6,19 +6,23 @@
  * 02/15/23 - added hamburger menu (AB)
  */
 
+//Open the menu when the hamburger is clicked
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", mobileMenu);
+hamburger.addEventListener("click", hamburgerMenu);
 
-function mobileMenu() {
+function hamburgerMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
 
+//Close menu when any navigation links are clicked
 const navLink = document.querySelectorAll(".nav-link");
+const logoLink = document.querySelector(".nav-logo");
 
 navLink.forEach(n => n.addEventListener("click", closeMenu));
+logoLink.addEventListener("click", closeMenu)
 
 function closeMenu(){
     hamburger.classList.remove("active");
