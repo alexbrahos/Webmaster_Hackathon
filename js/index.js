f3bdf4d97b9082f6d176008eb6470ba41207222d
Alex Brahos <alexanderbrahos@gmail.com>
2023-02-15 15:48:10 -0500

    added hamburger (needs documentation)

--- js/index.js
+++ js/index.js
@@ -3,6 +3,24 @@
  *
  * History:
  * 02/13/23 - created file (GN)
+ * 02/15/23 - added hamburger menu (AB)
  */
 
-console.log('HELLO WORLD');
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu(){
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
