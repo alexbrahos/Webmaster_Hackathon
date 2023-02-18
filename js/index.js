6029712f9b3da93914a0bcba37999673212d7444
Alex Brahos <alexanderbrahos@gmail.com>
2023-02-17 21:06:15 -0500

    did so much stuff like literally im not explaning

--- js/index.js
+++ js/index.js
@@ -5,6 +5,12 @@
  * 02/13/23 - created file (GN)
  * 02/15/23 - added hamburger menu (AB)
  * 02/15/23 - migrated hamburger code to menu.js (AB)
+ * 02/17/23 - added clicker game (AB)
  */
 
-console.log("Hello World");
+var clickerButton = document.getElementById("clickerButton");
+clickerButton.addEventListener("click", gotoClickerGame);
+
+function gotoClickerGame(){
+    window.location.href = "clicker.html";
+}
